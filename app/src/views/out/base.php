8b5e2f82284893733572c9aef78ac4132b84d61c
José Jesus <joseaugustolimajesus@gmail.com>
2024-06-09 18:54:47 -0300

    fix: create base out admin to user as template

---
+++ app/src/views/out/base.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $ck_title ?></title>
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+
+    <link rel="stylesheet" href="<?= css_directory("/bs5.css") ?>">
+    <link rel="stylesheet" href="<?= css_directory("/auth/login.css"); ?>">
+    <link rel="stylesheet" href="<?= css_directory("/notification.css") ?>">
+    <link rel="stylesheet" href="<?= css_directory("/out/base.css"); ?>">
+</head>
+
+<body>
+
+    <ul class="notificationsToasts"></ul>
+
+
+    <section class="p-3 p-md-4 p-xl-5" id="login">
+        <div class="container">
+           <?= $this->section("content"); ?>
+        </div>
+    </section>
+
+</body>
+
+
+<script src="<?= js_directory("/init.js") ?>"></script>
+<script src="<?= js_directory("/notification.js") ?>"></script>
+
+<?php viewingNotifications(); ?>
+
+</html>
